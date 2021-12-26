7d2de980998aa10d805c4fe5177b83d94cda2093
Phạm Văn Nam <57164967+phamnam1805@users.noreply.github.com>
2021-12-26 11:56:50 +0700

    update app config

--- src/main/java/com/tuana9a/controllers/NewsLetterSearchController.java
+++ src/main/java/com/tuana9a/controllers/NewsLetterSearchController.java
@@ -1,8 +1,11 @@
 package com.tuana9a.controllers;
 
+import com.tuana9a.config.AppConfig;
 import com.tuana9a.service.SearchEngineService;
 import lombok.AllArgsConstructor;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +21,10 @@ import java.util.Date;
 @RequestMapping("/api/search")
 @AllArgsConstructor
 public class NewsLetterSearchController {
-    SearchEngineService searchEngineService;
+
+    private SearchEngineService searchEngineService;
+
+    private AppConfig appConfig;
 
     @GetMapping
     public ResponseEntity<Object> search(@RequestParam("q") String q, @RequestParam("limit") Integer limit) throws ParseException, IOException {
@@ -27,16 +33,16 @@ public class NewsLetterSearchController {
 
     @PostMapping
     public ResponseEntity<Object> addDocuments(@RequestParam("file") MultipartFile file, @RequestParam("create") Boolean create) throws ParseException, IOException {
-        Path folder = Paths.get("./src/main/resources/static/data");
+        Path folder = Paths.get(appConfig.LUCENE_DOCUMENT_DIR);
         String fileName =  new Date().getTime() + "_" +file.getOriginalFilename();
         try {
             Files.copy(file.getInputStream(), folder.resolve(fileName));
-            File document = new File(fileName);
+            File document = new File(appConfig.LUCENE_DOCUMENT_DIR +"/"+fileName);
             searchEngineService.addDocuments(document, create);
+            return ResponseEntity.noContent().build();
         } catch (IOException e) {
             return ResponseEntity.status(500).body(e);
         }
-        return null;
     }
 
 }
