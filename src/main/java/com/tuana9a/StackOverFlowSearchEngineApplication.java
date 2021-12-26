ab1c84d74b46088e05243b6f6bbcc1ab02e8cc59
Phạm Văn Nam <57164967+phamnam1805@users.noreply.github.com>
2021-12-26 12:08:58 +0700

    add swagger

--- src/main/java/com/tuana9a/StackOverFlowSearchEngineApplication.java
+++ src/main/java/com/tuana9a/StackOverFlowSearchEngineApplication.java
@@ -6,18 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class StackOverFlowSearchEngineApplication implements CommandLineRunner {
-
-    @Autowired
-    private AppConfig config;
-
-    @Autowired
-    private SearchEngineService searchEngineService;
 
     public static void main(String[] args) {
         SpringApplication.run(StackOverFlowSearchEngineApplication.class, args);
@@ -26,5 +27,14 @@ public class StackOverFlowSearchEngineApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws IOException {
 
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
     }
 }
